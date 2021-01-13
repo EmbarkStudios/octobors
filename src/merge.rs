cb525664433f1be66454073da497ca3f99e3548a
Jake Shadle <jake.shadle@embark-studios.com>
2021-01-13 17:36:09 +0100

    Add merge abort reasons as comment to PR

--- src/merge.rs
+++ src/merge.rs
@@ -10,6 +10,8 @@ pub async fn queue(
     let pr_number = pr.number;
     let prh = crate::client_request!(client, pulls);
 
+    let ish = crate::client_request!(client, issues);
+
     let mut retry_count = 0u32;
 
     while retry_count < 3 {
@@ -39,28 +41,27 @@ pub async fn queue(
                 // already done so before our request, so if it didn't finish, we need
                 // to poll it again
                 log::warn!("Merge state for PR#{} is unknown, retrying", pr_number);
+
+                retry_count += 1;
+                tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+
+                continue;
             }
             Some(ms) => {
-                match ms {
-                    MergeableState::Draft => {
-                        log::warn!("PR#{} is a draft and can't be merged", pr_number,);
-                        return Ok(());
+                let abort_reason = match ms {
+                    MergeableState::Draft => Some("PR is a draft and can't be merged".to_owned()),
+                    MergeableState::Behind => Some(format!(
+                        "PR is behind '{}' and needs to be updated",
+                        pr.base.ref_field
+                    )),
+                    MergeableState::Dirty => {
+                        Some("Github is unable to create a merge commit for the PR".to_owned())
                     }
-                    MergeableState::Behind | MergeableState::Dirty => {
-                        log::warn!(
-                            "PR#{} is {:?} '{}' and needs to be updated",
-                            pr_number,
-                            ms,
-                            pr.base.ref_field
-                        );
-                        return Ok(());
+                    MergeableState::Blocked => {
+                        Some("1 or more required checks are pending".to_owned())
                     }
-                    MergeableState::Blocked | MergeableState::Unstable => {
-                        log::warn!(
-                            "PR#{} is blocked from merging by 1 or more pending or failed statuses",
-                            pr_number
-                        );
-                        return Ok(());
+                    MergeableState::Unstable => {
+                        Some("1 or more required checks have failed".to_owned())
                     }
                     MergeableState::Clean | MergeableState::HasHooks => {
                         let mut merge = prh
@@ -86,25 +87,34 @@ pub async fn queue(
                                     pr_number,
                                     res.sha.unwrap_or_default()
                                 );
-                            }
-                            Err(err) => {
-                                log::warn!("Failed to merge PR#{}: {:#}", pr_number, err);
-                            }
-                        }
 
-                        return Ok(());
+                                None
+                            }
+                            Err(err) => Some(format!("Failed to merge PR: {:#}", err)),
+                        }
                     }
                     MergeableState::Unknown => unreachable!(),
                     _ => {
                         log::warn!("Ignoring unknown merge state {:?} for PR#{}", ms, pr_number);
                         return Ok(());
                     }
+                };
+
+                if let Some(abort_reason) = abort_reason {
+                    log::warn!(
+                        "PR#{} was not able to automerge: {}",
+                        pr_number,
+                        abort_reason
+                    );
+
+                    let _ = ish
+                        .create_comment(pr_number, format!("automerge aborted: {}", abort_reason))
+                        .await;
                 }
             }
         }
 
-        retry_count += 1;
-        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+        return Ok(());
     }
 
     Ok(())
