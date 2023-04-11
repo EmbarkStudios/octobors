153c12fceb006b0996b339426e8be9dcf486dd11
David Pedersen <david.pdrsn@gmail.com>
2023-04-11 15:05:08 +0200

    Print error output on one line (#49)
    * Print error output on one line
    
    * maintain exit code

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@
 )]
 // END - Embark standard lints v0.4
 
-use std::path::PathBuf;
+use std::{path::PathBuf, process::ExitCode};
 
 use anyhow::{Context as _, Result};
 use log::metadata::LevelFilter;
@@ -76,7 +76,7 @@ use tracing as log;
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> ExitCode {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::builder()
@@ -86,6 +86,16 @@ async fn main() -> Result<()> {
         .without_time()
         .init();
 
+    match try_main().await {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            log::error!("Something went wrong: {err:#}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+async fn try_main() -> Result<()> {
     let app = octobors::Octobors::new(&get_config_path()?)?;
     log::info!("configuration: {:?}", app.config);
     app.process_all().await
